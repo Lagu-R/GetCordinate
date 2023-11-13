9e3537f2c17a8a987b17b6d354fd6c5539076bc4
Lagu-R <85457650+Lagu-R@users.noreply.github.com>
2023-11-13 19:16:43 +0400

    Create cordinate.py
    Gift from vakho.

---
+++ cordinate.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+while True:
+    x, y = pyautogui.position()
+    position_str = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    print(position_str, end='')
+    print('\b' * len(position_str), end='', flush=True)
